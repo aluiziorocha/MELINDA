2a7c00bd377c7e93e761bad8f1eadd0d1f955364
Aluizio Rocha <aluiziorocha@gmail.com>
2020-06-14 19:28:41 -0300

    Added queue example code

---
+++ queue-examples/queue-example.py
@@ -0,0 +1,30 @@
+import threading, queue
+
+# q = queue.SimpleQueue()
+q = queue.Queue()
+
+
+def worker():
+    while True:
+        # get the first item queued. It's blocking
+        # until there is an item on queue
+        item = q.get()
+        print(f'Working on {item}')
+        print(f'Finished {item}')
+        # remove item from queue when using q = queue.Queue()
+        q.task_done()
+
+
+# turn-on the worker thread
+threading.Thread(target=worker, daemon=True).start()
+
+# send ten task requests to the worker
+for item in range(10):
+    print(f'Queuing task {item}')
+    # put item on queue (as the last item)
+    q.put(item)
+print('All task requests sent\n', end='')
+
+# block until all tasks are done, when using q.task_done()
+q.join()
+print('All work completed')
