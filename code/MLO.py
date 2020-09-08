675d7fce2fef43003a88cae8874661894b56e887
Aluizio Rocha <aluiziorocha@gmail.com>
2020-09-08 10:14:11 -0300

    Sending JSON data instead node_name

--- code/MLO.py
+++ code/MLO.py
@@ -1,5 +1,6 @@
 # This is the template for the operator running a Measurement Level Task
-
+import datetime
+import json
 import socket
 import time
 import traceback
@@ -18,9 +19,13 @@ jpeg_quality = 90  # 0 to 100, higher is better quality, 95 is cv2 default
 try:
     while True:  # send images as stream until Ctrl-C
         image = camera.read()
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
         ret_code, jpg_buffer = cv2.imencode(
             ".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
-        sender.send_jpg(node_name, jpg_buffer)
+        jsondata = {'sensor_id': node_name,
+                    'timestamp': timestamp}
+        jsonstr = json.dumps(jsondata)
+        sender.send_jpg(jsonstr, jpg_buffer)
 
 except (KeyboardInterrupt, SystemExit):
     pass  # Ctrl-C was pressed to end program
