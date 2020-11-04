2da03483b6761ec07df2085e0eb18d79c2ded6a0
Aluizio Rocha <aluiziorocha@gmail.com>
2020-11-04 10:38:32 -0300

    Added functions Solutions and SelectNodes to Orchestrator.py

--- code/Orchestrator.py
+++ code/Orchestrator.py
@@ -1,34 +1,21 @@
 # This is the Orchestrator component managing the nodes allocation and deploying workflows
-
-from statistics import stdev
-
-# Weights (as costs) to select the best nodes combination to attend a workflow demand
-waste_weight = 4.0
-num_nodes_weight = 2.0
-std_fd_weight = 1.0
-
-# Idle nodes available for processing a new workflow
-nodes = [{'edgeNodeId': '5f2484b37c803900291ea3d0', 'mlo': 5, 'flo': 20, 'dlo': 30},
-         {'edgeNodeId': '5f2484b37c803900291ea3d1', 'mlo': 1, 'flo': 10, 'dlo': 50},
-         {'edgeNodeId': '5f2484b37c803900291ea3d2', 'mlo': 10, 'flo': 25, 'dlo': 90},
-         {'edgeNodeId': '5f2484b37c803900291ea3d3', 'mlo': 40, 'flo': 30, 'dlo': 10},
-         {'edgeNodeId': '5f2484b37c803900291ea3d4', 'mlo': 3, 'flo': 20, 'dlo': 10},
-         {'edgeNodeId': '5f2484b37c803900291ea3d5', 'mlo': 20, 'flo': 10, 'dlo': 10}]
-
-# Workflow demand in FPS
-workflow_fps_demand = 45
+import argparse
+from math import inf
+from os import path
 
 
-def SelectNodes(fps_demand, nodes):
-    nodes = sorted(nodes.items(), key=lambda x: x[1], reverse=True)
+def Solutions(fps_demand, nodes):
+    nodes.sort(key=lambda x: x[1], reverse=True)
     solutions = []
-    for pivot in range(len(nodes)):
+    k = len(nodes)
+    for pivot in range(k):
         sum_capacity = nodes[pivot][1]
         partial_solution = [nodes[pivot]]
         if sum_capacity >= fps_demand:
             solutions.append(partial_solution)
         else:
-            for i in range(pivot + 1, len(nodes)):
+            for i in range(pivot + 1, k):
+                # --- Searching a node with remaining capacity
                 cap = fps_demand - sum_capacity
                 try:
                     index = [x[1] for x in nodes[i:]].index(cap) + i
@@ -39,6 +26,7 @@ def SelectNodes(fps_demand, nodes):
                     partial_solution.append(nodes[index])
                     solutions.append(partial_solution)
                     break
+                # -----------
                 cap = sum_capacity + nodes[i][1]
                 if cap >= fps_demand:
                     possible_solution = partial_solution.copy()
@@ -47,46 +35,62 @@ def SelectNodes(fps_demand, nodes):
                 else:
                     sum_capacity += nodes[i][1]
                     partial_solution.append(nodes[i])
+    return solutions
 
+
+def SelectNodes(fps_demand, nodes, A, B, C, D):
+    solutions = Solutions(fps_demand, nodes)
     if len(solutions) > 0:
-        best_solution = best_solution_cost = 99999
+        best_solution = best_solution_cost = inf
         for i, combination in enumerate(solutions):
             total_fps = sum([element[1] for element in combination])
-            waste = total_fps - workflow_fps_demand
-            num_nodes = len(combination)
-            frames_distribution = 0 if len(combination) == 1 else stdev([element[1] for element in combination])
-            cost = waste * waste_weight + num_nodes * num_nodes_weight + frames_distribution * std_fd_weight
+            wst = total_fps - fps_demand
+            lat = sum([element[2] for element in combination])
+            ene = sum([element[3] for element in combination])
+            ava = sum([1 - element[4] for element in combination])
+            cost = wst * A + lat * B + ene * C + ava * D
             if cost < best_solution_cost:
                 best_solution_cost = cost
                 best_solution = i
-        return solutions[best_solution]
+        return solutions[best_solution], best_solution_cost
     else:
-        return solutions
+        return solutions, 0
 
 
-def main():
-    mlo_nodes = {}
-    for node in nodes:
-        mlo_nodes[node['edgeNodeId']] = node['mlo']
-    mlo_nodes_selected = SelectNodes(workflow_fps_demand, mlo_nodes)
+def main(args):
+    if path.isfile(args["datafile"]):
+        nodes = []
+        with open(args['datafile']) as fp:
+            line = fp.readline()
+            while line:
+                col = line.split()
+                if len(col) > 1:
+                    nodes.append((col[0], int(col[1]), int(col[2]), int(col[3]), float(col[4])))
+                line = fp.readline()
 
-    flo_nodes = {}
-    for node in nodes:
-        if node['edgeNodeId'] not in [x[0] for x in mlo_nodes_selected]:
-            flo_nodes[node['edgeNodeId']] = node['flo']
-    flo_nodes_selected = SelectNodes(workflow_fps_demand, flo_nodes)
+    # Workflow demand in FPS
+    workflow_fps_demand = args["fps"]
 
-    dlo_nodes = {}
-    for node in nodes:
-        if node['edgeNodeId'] not in [x[0] for x in mlo_nodes_selected] and \
-           node['edgeNodeId'] not in [x[0] for x in flo_nodes_selected]:
-            dlo_nodes[node['edgeNodeId']] = node['dlo']
-    dlo_nodes_selected = SelectNodes(workflow_fps_demand, dlo_nodes)
-    print("Nodes selected to attend a workflow demanding {} FPS of processing capacity:".format(workflow_fps_demand))
-    print("MLO operators: {}".format(mlo_nodes_selected))
-    print("FLO operators: {}".format(flo_nodes_selected))
-    print("DLO operators: {}".format(dlo_nodes_selected))
+    # Weights prioritizing QoS requirements
+    A = args["A"]
+    B = args["B"]
+    C = args["C"]
+    D = args["D"]
+
+    nodes_selected, cost = SelectNodes(workflow_fps_demand, nodes, A, B, C, D)
+    print("To process {} FPS, cost is {} with the following nodes: {}".format(workflow_fps_demand, cost,
+                                                                              [x[0] for x in nodes_selected]))
+
 
 
 if __name__ == "__main__":
-    main()
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-df", "--datafile", required=True, help="File containing nodes data")
+    ap.add_argument("-f", "--fps", type=int, required=True, help="Workflow demand in FPS")
+    ap.add_argument("-a", "--A", type=float, default=0.25, help="Coefficient A")
+    ap.add_argument("-b", "--B", type=float, default=0.25, help="Coefficient B")
+    ap.add_argument("-c", "--C", type=float, default=0.25, help="Coefficient C")
+    ap.add_argument("-d", "--D", type=float, default=0.25, help="Coefficient D")
+    args = vars(ap.parse_args())
+
+    main(args)
